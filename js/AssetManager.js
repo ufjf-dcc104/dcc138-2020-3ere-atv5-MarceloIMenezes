0b37e7b72e9279ef521269165e90ea3617991b89
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-03-11 17:59:58 -0300

    coloca funcao para carregar e retornar imagem

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -2,6 +2,21 @@ export default class AssetManager {
     constructor() {
         this.nTotalImg = 0;
         this.nCarregado = 0;
+
+        this.imagens = new Map();
+    }
+
+    carregaImagem(chave, src) {
+        const img = new Image();
+        img.addEventListener("load", () => {
+            nCarregado++;
+        });
+        img.src = src;
+        this.imagens.set(chave, img);
+        this.nTotalImg++;
+    }
+    imagem(chave) {
+        return this.imagens.get(chave);
     }
 
     porcentagemCarregada() {
