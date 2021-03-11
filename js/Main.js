6f124596382f11e6a7916c6a4a4b79f6ff07db84
Marcelo Ian <marcelo.menezes@estudante.ufjf.br>
2021-03-11 17:44:51 -0300

    refatora main para apresentar menu

--- js/Main.js
+++ js/Main.js
@@ -1,21 +1,29 @@
+import AssetManager from "./AssetManager.js";
 import Cena from "./Cena.js";
 import Sprite from "./Sprite.js";
 
 const canvas = document.querySelector("canvas");
 
-const cena = new Cena(canvas);
+const asset = new AssetManager()
+const cena = new Cena(canvas, asset);
 const s1 = new Sprite();
 
 cena.addSprite(s1);
 cena.addSprite(new Sprite({x: 60, vx: -10, color: "blue"}));
 
-document.addEventListener("keydown", (e) => {
-    switch (e.key) {
-        case "p":
-        case "P":
-            cena.parar();
-            break;
-        default:
-            cena.iniciar();
-    }
-});
+cena.iniciarMenu();
+if (asset.porcentagemCarregada() == 100) {
+    cena.parar();
+    cena.iniciar();
+
+    document.addEventListener("keydown", (e) => {
+        switch (e.key) {
+            case "p":
+            case "P":
+                cena.parar();
+                break;
+            default:
+                cena.iniciar();
+        }
+    });
+}
